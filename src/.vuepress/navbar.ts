91fb42611d55c7f54b56c512b4fa7a87f38dd72a
爱喝水的木子 <50564847+ice-a@users.noreply.github.com>
2023-11-06 17:58:25 +0800

    add navbar.ts Signed-off-by: 爱喝水的木子 <50564847+ice-a@users.noreply.github.com>

--- src/.vuepress/navbar.ts
+++ src/.vuepress/navbar.ts
@@ -2,19 +2,11 @@ import { navbar } from "vuepress-theme-hope";
 
 export default navbar([
   "/",
-  // "/demo/",
-  // "/posts/",
   {
     text: "博文",
     icon: "blog",
     link: "/posts/",
   },
-  // "/door/",
-  // {
-  //   text: "V2 文档",
-  //   icon: "book",
-  //   link: "https://theme-hope.vuejs.press/zh/",
-  // },
   {
     text: "主站",
     icon: "book",
