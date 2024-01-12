4d81d6ca718a11c486f03ce8949ad957a165f78a
爱喝水的木子 <50564847+ice-a@users.noreply.github.com>
2024-01-12 18:21:11 +0800

    Update config.ts

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -15,7 +15,7 @@ export default defineUserConfig({
     //   rss:true,
     // }),
     // copyCodePlugin({}), // 代码复制
-
+      'umami': { trackerUrl: 'https://umami.lideshan.top', siteId: '6d386c85-09bd-4369-a3b1-4a2b24690b8c' }
   ],
   // Enable it with pwa
   // shouldPrefetch: false,
